aede882f301b0f2cf777efbe3e0f1ecf2ea7e5fa
RMeissnerCC <robert.meissner@codecentric.de>
2023-05-17 12:29:18 +0200

    Solved Day 4 first task

---
+++ src/main/kotlin/aoc2022/day4.kt
@@ -0,0 +1,34 @@
+package aoc2022
+
+import utils.loadData
+
+fun main() {
+
+    val fileName = "src/main/resources/aoc2022/day4"
+    val assignments = loadData(fileName)
+
+    val result = firstTask(assignments)
+}
+
+fun firstTask(data: List<String>): Int {
+    var count = 0
+    for (pair in data) {
+        val pairs: List<String> = pair.split(",")
+        val elfs = mutableListOf<List<Int>>()
+        pairs.map { it ->
+            val elf = it.split("-").map { it.toInt() }
+            elfs.add(elf[0].rangeTo(elf[1]).toList())
+        }
+        count += if (isSubset(elfs)) 1 else 0
+        println("elfs: $pairs -> $count")
+        println(count)
+    }
+    return count
+}
+
+fun isSubset(pairs: MutableList<List<Int>>): Boolean {
+    val intersection = pairs[0].intersect(pairs[1].toSet()).toSet()
+    return intersection == pairs[1].toSet() || intersection == pairs[0].toSet()
+}
+
+
---
+++ src/test/kotlin/aoc2022/Day4KtTest.kt
@@ -0,0 +1,40 @@
+package aoc2022
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import utils.loadData
+
+class Day4KtTest {
+    @Test
+    fun testFirstTask() {
+        val data = "2-4,6-8\n" +
+                "2-3,4-5\n" +
+                "5-7,7-9\n" +
+                "2-8,3-7\n" +
+                "6-6,4-6\n" +
+                "2-6,4-8"
+        var assignments = data.split("\n")
+        assertEquals(2, firstTask(assignments))
+
+
+        val fileName = "src/main/resources/aoc2022/day4"
+        assignments = loadData(fileName)
+        assertEquals(534, firstTask(assignments))
+    }
+
+    @Test
+    fun testisSubset() {
+        var elfs = mutableListOf(intArrayOf(0, 1, 2).toList(), intArrayOf(3).toList())
+        assertEquals(false, isSubset(elfs))
+
+        elfs = mutableListOf(intArrayOf(0, 1, 2).toList(), intArrayOf(1).toList())
+        assertEquals(true, isSubset(elfs))
+
+        elfs = mutableListOf(intArrayOf(0, 1, 2).toList(), intArrayOf(1, 2, 3).toList())
+        assertEquals(false, isSubset(elfs))
+
+        elfs = mutableListOf(intArrayOf(0, 1, 2).toList(), intArrayOf(0,1,2).toList())
+        assertEquals(true, isSubset(elfs))
+
+    }
+}
