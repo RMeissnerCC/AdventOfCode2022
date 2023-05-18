d7a603792295bf1dd02900631069812740cac6a1
Robert Meissner <robert.meissner@codecentric.de>
2023-05-18 17:41:17 +0200

    WIP: Working day 5

--- src/main/kotlin/aoc2022/day5.kt
+++ src/main/kotlin/aoc2022/day5.kt
@@ -2,43 +2,80 @@ package aoc2022
 
 import utils.loadData
 
-typealias Warehouse = Map<Int, String>
+typealias Crate = String
+typealias Warehouse = Map<Int, Crate>
+typealias MoveInstruction = MutableList<Triple<Int, Int, Int>>
 
+val moveRegex = "move\\s(\\d+)\\sfrom\\s(\\d)\\sto\\s(\\d)".toRegex()
+val warehouseRegex = "[\\[\\s?]([\\w\\s?])[]\\s?]\\s?".toRegex()
 fun main() {
 
     val fileName = "src/main/resources/aoc2022/day5"
     val assignments = loadData(fileName)
-
-    // parse crate setup
-    for (line in assignments) {
-        println(line)
-        println(line.chunked(4))
-    }
-
-    // parse move instructions
-
-    var warehouse = mapOf(1 to "CMK", 2 to "KDM")
-    // apply move instructions
-    // Try to: move 1 from 1 to 2
-    warehouse = warehouse.move(1, 1, 2)
-
-
-    // receive top crates, i.e., the crates at the beginning of each list
-    println(warehouse)
-    val output = warehouse.getTopCrates()
+    val output = day5Task1(assignments)
     println("Final order of crates: $output")
 }
 
+fun day5Task1(assignments: List<String>): String {
+    // parse crate setup
+    var warehouse = parseWarehouseSetup(assignments)
+
+    val moveInstructions: MoveInstruction = parseMoveInstruction(assignments)
+
+    for (move in moveInstructions) {
+        warehouse = warehouse.move(move.first, move.second, move.third)
+    }
+    // receive top crates, i.e., the crates at the beginning of each list
+    return warehouse.getTopCrates()
+}
+
+fun parseWarehouseSetup(assignments: List<String>): Map<Int, String> {
+    val warehouse = mutableMapOf(1 to "", 2 to "", 3 to "", 4 to "", 5 to "", 6 to "", 7 to "", 8 to "", 9 to "")
+    for (line in assignments) {
+        if (line == " 1   2   3   4   5   6   7   8   9") break
+        val match = warehouseRegex.findAll(line).toList()
+        for (column in warehouse.keys) {
+            if (match.size >= column) {
+                val currentGroup = match[column - 1].groups[0]?.value?.get(1).toString()
+                println(currentGroup)
+                if (currentGroup != " ") {
+                    warehouse[column] = currentGroup + warehouse[column]
+                    println(warehouse)
+                }
+            }
+
+        }
+    }
+    return warehouse
+}
+
+fun parseMoveInstruction(
+    assignments: List<String>
+): MoveInstruction {
+    val moveInstructions: MoveInstruction = mutableListOf()
+    for (line in assignments) {
+        val match = moveRegex.matchEntire(line)
+        if (match != null) {
+            moveInstructions.add(
+                Triple(
+                    match.groups[1]!!.value.toInt(),
+                    match.groups[2]!!.value.toInt(),
+                    match.groups[3]!!.value.toInt()
+                )
+            )
+        }
+    }
+    return moveInstructions
+}
+
+
 fun Warehouse.move(number: Int, from: Int, to: Int): Warehouse {
-    var newWarehouse = this.toMutableMap()
-    println(newWarehouse[from])
-    println(newWarehouse[to])
-    println(newWarehouse[from]?.slice(0 until number))
-
-    // Todo how to split here?!
-//    newWarehouse[from] = newWarehouse[from].slice(number .. newWarehouse[from].length)
-    newWarehouse[to] = newWarehouse[from]?.slice(0 until number) + newWarehouse[to]
-
+    val newWarehouse = this.toMutableMap()
+    for (count in 1..number) {
+        val movedCrate: Crate = newWarehouse[from]!![0].toString()
+        newWarehouse[from] = newWarehouse[from]!!.slice(1 until newWarehouse[from]!!.length)
+        newWarehouse[to] = movedCrate + newWarehouse[to]
+    }
     return newWarehouse
 }
 
--- src/test/kotlin/aoc2022/Day5KtTest.kt
+++ src/test/kotlin/aoc2022/Day5KtTest.kt
@@ -2,6 +2,7 @@ package aoc2022
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import utils.loadData
 
 class Day5KtTest {
     @Test
@@ -9,13 +10,61 @@ class Day5KtTest {
         val warehouse: Warehouse = mapOf(1 to "CMK", 2 to "MK", 3 to "ZDAA")
         assertEquals("CMZ", warehouse.getTopCrates())
     }
+
     @Test
     fun testMove() {
         val warehouse: Warehouse = mapOf(1 to "CMK", 2 to "GY", 3 to "ZDAA")
-        assertEquals(warehouse, warehouse.move(1,1,1)) // identity transformation
+        assertEquals(warehouse, warehouse.move(1, 1, 1)) // identity transformation
 
-        val movedWarehouse: Warehouse = mapOf(1 to "MK", 2 to "CGY", 3 to "ZDAA")
-        assertEquals(movedWarehouse, warehouse.move(1,1,2)) // identity transformation
+        var movedWarehouse: Warehouse = mapOf(1 to "MK", 2 to "CGY", 3 to "ZDAA")
+        assertEquals(movedWarehouse, warehouse.move(1, 1, 2))
+
+        movedWarehouse = mapOf(1 to "K", 2 to "MCGY", 3 to "ZDAA")
+        assertEquals(movedWarehouse, warehouse.move(2, 1, 2))
+        movedWarehouse = mapOf(1 to "", 2 to "GY", 3 to "KMCZDAA")
+        assertEquals(movedWarehouse, warehouse.move(3, 1, 3))
+    }
+
+    @Test
+    fun testParseMoveInstruction() {
+        var moveInstructionRaw = listOf("move 3 from 5 to 2")
+        assertEquals(
+            listOf(Triple(3, 5, 2)),
+            parseMoveInstruction(moveInstructionRaw)
+        )
+
+        moveInstructionRaw = listOf("move 3 from 5 to 2", "move 12 from 1 to 9", "no regex match")
+        assertEquals(
+            listOf(Triple(3, 5, 2), Triple(12, 1, 9)),
+            parseMoveInstruction(moveInstructionRaw)
+        )
+
+    }
+
+    @Test
+    fun testParseWarehouseSetup() {
+        val moveInstructionRaw = listOf("[G] [Z] [C] [H] [C] [R] [H] [P] [D]", "        [F] [Q]         [Q]")
+        val warehouse =
+            mapOf(1 to "G", 2 to "Z", 3 to "FC", 4 to "QH", 5 to "C", 6 to "R", 7 to "QH", 8 to "P", 9 to "D")
+        assertEquals(
+            warehouse,
+            parseWarehouseSetup(moveInstructionRaw)
+        )
+
+
+    }
+
+    @Test
+    fun testFirstTask() {
+        val resultIsNot = "SFPBQDGZV"
+
+        val fileName = "src/main/resources/aoc2022/day5"
+        val assignments = loadData(fileName)
+        assertNotEquals(
+            resultIsNot,
+            day5Task1(assignments)
+        )
+
     }
 
 
