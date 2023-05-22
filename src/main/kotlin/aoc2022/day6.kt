a70d47aff3c35708bf6328df418159bd7ae8869c
RMeissnerCC <robert.meissner@codecentric.de>
2023-05-22 10:31:38 +0200

    WIP: day 6

---
+++ src/main/kotlin/aoc2022/day6.kt
@@ -0,0 +1,11 @@
+package aoc2022
+
+import utils.loadData
+
+fun main() {
+
+    val fileName = "src/main/resources/aoc2022/day6"
+    val signals = loadData(fileName)
+
+    // parse data as sets? sliding window, with only unique characters?
+}
